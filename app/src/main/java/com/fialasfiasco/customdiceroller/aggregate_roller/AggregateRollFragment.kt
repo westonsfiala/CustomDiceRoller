ca94d063f0db60604b3050d97f75431e28026dcf
wesfiala <weston.fiala@keysight.com>
2019-07-19 15:27:33 -0700

    Added a todo message when you click the save button

--- app/src/main/java/com/fialasfiasco/customdiceroller/aggregate_roller/AggregateRollFragment.kt
+++ app/src/main/java/com/fialasfiasco/customdiceroller/aggregate_roller/AggregateRollFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -115,6 +116,7 @@ class AggregateRollFragment : Fragment(),
     {
         saveButton.setOnClickListener {
             //val aggregateDieList = getAggregateDies()
+            Toast.makeText(context, "TODO", Toast.LENGTH_SHORT).show()
         }
     }
 
