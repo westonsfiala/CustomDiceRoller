a074a14a09293c556d16d2518da165925a29e0e8
westonsfiala <westonsfiala@gmail.com>
2019-10-01 19:46:34 -0700

    Fixed the expand/contract imaging so that it doesn't do weird things when you leave the page when things are expanded.

--- app/src/main/java/com/fialasfiasco/customdiceroller/saved_roller/SavedRollerCategoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/fialasfiasco/customdiceroller/saved_roller/SavedRollerCategoryRecyclerViewAdapter.kt
@@ -5,7 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
+import android.view.animation.ScaleAnimation
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,8 +14,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.fialasfiasco.customdiceroller.R
 import com.fialasfiasco.customdiceroller.data.PageViewModel
 import kotlinx.android.synthetic.main.holder_saved_roll_category.view.*
-
-
 
 /**
  * [RecyclerView.Adapter] that can display a [SavedRollCategoryViewHolder]
@@ -40,68 +39,73 @@ class SavedRollerCategoryRecyclerViewAdapter(private val context: Context,
     }
 
     private fun setupShowHideButton(holder: SavedRollCategoryViewHolder, categoryName: String) {
+
+        val contractCategories = ScaleAnimation(1f,1f, 1f, 0f, 0f, 0f)
+
+        contractCategories.duration = 300
+
+        contractCategories.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(arg0: Animation) {}
+            override fun onAnimationRepeat(arg0: Animation) {}
+            override fun onAnimationEnd(arg0: Animation) {
+                holder.mInnerRecyclerView.visibility = RecyclerView.GONE
+            }
+        })
+
+        val expandCategories = ScaleAnimation(1f,1f, 0f, 1f, 0f, 0f)
+
+        expandCategories.duration = 300
+
+        expandCategories.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(arg0: Animation) {
+                holder.mInnerRecyclerView.visibility = RecyclerView.VISIBLE
+            }
+            override fun onAnimationRepeat(arg0: Animation) {}
+            override fun onAnimationEnd(arg0: Animation) {}
+        })
+
+        val isExpanded = pageViewModel.getCategoryExpanded(categoryName)
+
+        // You have to do this because it deals with the whole activity being reloaded, and the fragment being reloaded.
+        // If you don't it will have small glitches when you either go to far away tabs or close and reopen the app.
+        holder.mShowHideImage.rotation = if(isExpanded) {180f} else {90f}
+        val rotateStart = if(isExpanded) {-90f} else {0f}
+        val rotateEnd = if(isExpanded) {0f} else {90f}
+
+        val rotateHideShow = RotateAnimation(rotateStart, rotateEnd,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f)
+
+        rotateHideShow.duration = 300
+        rotateHideShow.fillAfter = true
+
+        val unRotateHideShow = RotateAnimation(rotateEnd, rotateStart,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f)
+
+        unRotateHideShow.duration = 300
+        unRotateHideShow.fillAfter = true
+
         holder.mShowHideLayout.setOnClickListener {
             if(holder.mInnerRecyclerView.visibility == RecyclerView.VISIBLE) {
-                val rotateHideShow = AnimationUtils.loadAnimation(context, R.anim.unrotate_saved_roll_categories)
 
-                rotateHideShow.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationStart(arg0: Animation) {}
-                    override fun onAnimationRepeat(arg0: Animation) {}
-                    override fun onAnimationEnd(arg0: Animation) {
-                        //holder.mShowHideImage.rotation = 90f
-                    }
-                })
-
-                holder.mShowHideImage.startAnimation(rotateHideShow)
-
-                val contractCategories = AnimationUtils.loadAnimation(context, R.anim.contract_saved_roll_categories)
-
-                contractCategories.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationStart(arg0: Animation) {}
-                    override fun onAnimationRepeat(arg0: Animation) {}
-                    override fun onAnimationEnd(arg0: Animation) {
-                        holder.mInnerRecyclerView.visibility = RecyclerView.GONE
-                    }
-                })
-
+                holder.mShowHideImage.startAnimation(unRotateHideShow)
                 holder.mInnerRecyclerView.startAnimation(contractCategories)
 
                 pageViewModel.setCategoryExpanded(categoryName, false)
             } else {
 
-                val rotateHideShow = AnimationUtils.loadAnimation(context, R.anim.rotate_saved_roll_categories)
-
-                rotateHideShow.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationStart(arg0: Animation) {}
-                    override fun onAnimationRepeat(arg0: Animation) {}
-                    override fun onAnimationEnd(arg0: Animation) {
-                        //holder.mShowHideImage.rotation = 180f
-                    }
-                })
-
                 holder.mShowHideImage.startAnimation(rotateHideShow)
-
-                val expandCategories = AnimationUtils.loadAnimation(context, R.anim.expand_saved_roll_categories)
-
-                expandCategories.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationStart(arg0: Animation) {
-                        holder.mInnerRecyclerView.visibility = RecyclerView.VISIBLE
-                    }
-                    override fun onAnimationRepeat(arg0: Animation) {}
-                    override fun onAnimationEnd(arg0: Animation) {}
-                })
-
                 holder.mInnerRecyclerView.startAnimation(expandCategories)
 
                 pageViewModel.setCategoryExpanded(categoryName, true)
             }
         }
 
+
         if(pageViewModel.getCategoryExpanded(categoryName)) {
-            holder.mShowHideImage.rotation = 180f
             holder.mInnerRecyclerView.visibility = RecyclerView.VISIBLE
         } else {
-            holder.mShowHideImage.rotation = 90f
             holder.mInnerRecyclerView.visibility = RecyclerView.GONE
         }
     }
